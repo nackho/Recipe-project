1cef02b20b82cc742c4f20439faeb62b533d1dd4
nackho <nick_ho1011@yahoo.com>
2022-10-07 23:40:20 -0700

    Added OAuth functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
-
+const passport = require("passport")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//Google OAuth login route
+router.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"],
+  promt: "select_account"
+}))
+
+//Google OAuth callback route
+router.get("/oauth2callback", passport.authenticate(
+  "google", 
+  {
+    successRedirect: "/movies",
+    failureRedirect: "/movies"
+  }
+))
+
+//Google OAuth logout route
+router.get("/logout", function(req, res, next) {
+  req.logout(function(err) {
+    if(err) return next(err)
+      res.redirect("/movies")
+  })
+})
 
 module.exports = router;
