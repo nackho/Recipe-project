3a8b3a12f2c472dd08186cfd129144ccb3ec9c78
nackho <nick_ho1011@yahoo.com>
2022-10-10 22:40:21 -0700

    updated new recipe form

--- models/recipe.js
+++ models/recipe.js
@@ -3,12 +3,26 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema ({
-    title: String,
-    source: String,
-    description: String, 
-    ingredients: [String],
-    method: [String],
-    tips: String
-})
+    image: {
+        type: String,
+        required: true
+    },
+    title: { 
+        type: String, 
+        required: true
+    },
+    description: { 
+        type: String, 
+        required: true 
+    },
+    ingredients: { 
+        type: [String], 
+        required: true 
+    },
+    method: { 
+        type: [String], 
+        required: true 
+    }
+});
 
 module.exports = mongoose.model("Recipe", recipeSchema)
