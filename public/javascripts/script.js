9f44f974974871dc62ba337fcf2eb74204b8cb64
nackho <nick_ho1011@yahoo.com>
2022-10-11 23:35:44 -0700

    Implemented Add/Remove Ingredient/Method functionality.

---
+++ public/javascripts/script.js
@@ -0,0 +1,43 @@
+const recipeIngredient = document.getElementById("ingredientItem");
+let addIngredientBtn = document.getElementById("addIngredientBtn");
+let removeIngredientBtn = document.getElementById("removeIngredientBtn");
+
+function addIngredient(){
+    let input = document.createElement("input");
+    button = document.createElement("button");
+    button.innerHTML = "X";
+    button.onclick = removeIngredient;
+    recipeIngredient.appendChild(input);
+    recipeIngredient.appendChild(button)
+}
+
+function removeIngredient() {
+    recipeIngredient.removeChild(this.previousElementSibling);
+    recipeIngredient.removeChild(this);
+}
+
+addIngredientBtn.addEventListener("click", addIngredient);
+removeIngredientBtn.addEventListener("click", removeIngredient)
+
+
+const recipeMethod = document.getElementById("methodItem");
+let addMethodBtn = document.getElementById("addMethodBtn");
+let removeMethodBtn = document.getElementById("removeMethodBtn");
+
+function addMethod(){
+    let input = document.createElement("input");
+    button = document.createElement("button");
+    button.innerHTML = "X";
+    input.placeholder = "What's next?";
+    button.onclick = removeMethod;
+    recipeMethod.appendChild(input);
+    recipeMethod.appendChild(button)
+}
+
+function removeMethod() {
+    recipeMethod.removeChild(this.previousElementSibling);
+    recipeMethod.removeChild(this);
+}
+
+addMethodBtn.addEventListener("click", addMethod);
+removeMethodBtn.addEventListener("click", removeMethod)
