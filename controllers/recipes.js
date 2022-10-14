ed2166ff2fc9d7ae6ac729fc1544cae1602fa212
nackho <nick_ho1011@yahoo.com>
2022-10-13 20:14:48 -0700

    Completed show page functionality

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -29,7 +29,7 @@ function create(req, res) {
 }
 
 function show(req,res) {
-    Recipe.findById(req.params.id, function(err, recipes) {
-        res.render("recipes/show", { title: "test", recipes })
+    Recipe.findById(req.params.id, function(err, recipe) {
+        res.render("recipes/show", { title: "Recipe", recipe })
     })
 }
